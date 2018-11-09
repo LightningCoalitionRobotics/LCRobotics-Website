b17c758c0736f3d863dfcb58e0f6d1d0602afebc
Lionclaw49 <griffindugan49@gmail.com>
2018-11-09 10:45:12 -0500

    Trying to get the scroll to work...

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -39,3 +39,11 @@
   }
 
 })(jQuery); // End of use strict
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
